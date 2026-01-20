fe5a269ba4af12241720ed9756c052e802453841
ifranahamed88-dot <ifranahamed88@gmail.com>
2026-01-20 21:48:12 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+
+export const CONFIG = {
+  VERIFY_TOKEN: "ifran123",
+  ADMIN_PASSWORD: "admin123",
+
+  // ⚠️ Replace TEMP later with real Page Access Token
+  PAGE_ACCESS_TOKEN: "TEMP",
+
+  // Default DM sent to everyone
+  DM_TEXT: "Thanks for commenting ✅ Here’s the link: https://yourlink.com"
+};
